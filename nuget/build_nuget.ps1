6fc73ba256bf362a4ff4a0816fb69cc37e2b054d
Ivan Gagis <igagis@gmail.com>
2019-10-28 16:58:37 +0200

    travis

--- nuget/build_nuget.ps1
+++ nuget/build_nuget.ps1
@@ -2,6 +2,21 @@ Push-Location
 $scriptdir = Split-Path $MyInvocation.MyCommand.Path
 cd $scriptdir
 
+#extract version from debian/changelog
+$ver = (Get-Content ..\version.txt -Head 1) -replace ".*\((\d*\.\d*\.\d*)(\-\d+){0,1}\).*",'$1'
+#Write-Host $ver
+
+#insert version into all *.in files
+Get-ChildItem "." -Filter *.in | Foreach-Object{
+    $content = Get-Content $_.FullName
+
+    #filter and save content to the original file
+    #$content | Where-Object {$_ -match 'step[49]'} | Set-Content $_.FullName
+
+    #filter and save content to a new file
+    ($content -replace '\$\(version\)',"$ver") | Set-Content ($_.BaseName)
+}
+
 
 "%VS142COMNTOOLS%\VsMSBuildCmd.bat"
 
